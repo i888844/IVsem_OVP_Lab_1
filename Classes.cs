04776c6aa022d96d742b95b1bed86bf823bc4bb3
i888844 <i888844@yandex.ru>
2024-02-15 22:12:05 +0300

    2nd commit

---
+++ Classes.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab._1
+{
+    internal class Classes
+    {
+        public class ComplexNumber
+        {
+            public double A { get; private set; }
+            public double B { get; private set; }
+
+            public ComplexNumber()
+            {
+                A = 0.0;
+                B = 0.0;
+            }
+
+            public ComplexNumber(double valueA, double valueB)
+            {
+                A = valueA;
+                B = valueB;
+            }
+
+            public ComplexNumber(ComplexNumber sourceComplexNumber)
+            {
+                if (this != sourceComplexNumber)
+                {
+                    A = sourceComplexNumber.A;
+                    B = sourceComplexNumber.B;
+                }
+            }
+
+            ~ComplexNumber() { }
+
+            public override string ToString()
+            {
+                if (A >= 0) return $"{A} + {B}i";
+                else return $"{A} - {Math.Abs(B)}i";
+            }
+        }
+
+        public class ComplexNumberOperator
+        {
+            private ComplexNumber[] complexNumbers;
+
+            public ComplexNumberOperator()
+            {
+                complexNumbers = new ComplexNumber[2];
+                complexNumbers[0] = new ComplexNumber();
+                complexNumbers[1] = new ComplexNumber();
+            }
+
+            public ComplexNumberOperator(double firstComplexNumberA, double firstComplexNumberB,
+                double secondComplexNumberA, double secondComplexNumberB)
+            {
+                complexNumbers = new ComplexNumber[2];
+                complexNumbers[0] = new ComplexNumber(firstComplexNumberA, firstComplexNumberB);
+                complexNumbers[1] = new ComplexNumber(secondComplexNumberA, secondComplexNumberB);
+            }
+
+            public ComplexNumberOperator(ComplexNumberOperator sourceCompexNumberOperator)
+            {
+                if (this != sourceCompexNumberOperator)
+                {
+                    complexNumbers = new ComplexNumber[2];
+                    complexNumbers[0] = new ComplexNumber(sourceCompexNumberOperator.complexNumbers[0]);
+                    complexNumbers[1] = new ComplexNumber(sourceCompexNumberOperator.complexNumbers[1]);
+                }
+            }
+
+            ~ComplexNumberOperator()
+            {
+                if (complexNumbers != null)
+                {
+                    complexNumbers[0] = null;
+                    complexNumbers[1] = null;
+                }
+            }
+
+            public ComplexNumber[] СomplexNumbers
+            {
+                get { return complexNumbers; }
+                set
+                {
+                    if (value.Length != 2)
+                    {
+                        throw new ArgumentException("Массив должен иметь два элемента.");
+                    }
+
+                    complexNumbers = new ComplexNumber[2];
+                    complexNumbers[0] = new ComplexNumber(value[0]);
+                    complexNumbers[1] = new ComplexNumber(value[1]);
+                }
+            }
+
+            public ComplexNumber Add()
+            {
+                double resultA = complexNumbers[0].A + complexNumbers[1].A;
+                double resultB = complexNumbers[0].B + complexNumbers[1].B;
+
+                return new ComplexNumber(resultA, resultB);
+            }
+
+            public ComplexNumber Subtract()
+            {
+                double resultA = complexNumbers[0].A - complexNumbers[1].A;
+                double resultB = complexNumbers[0].B - complexNumbers[1].B;
+
+                return new ComplexNumber(resultA, resultB);
+            }
+
+            public ComplexNumber Multiply()
+            {
+                double resultA = complexNumbers[0].A * complexNumbers[1].A - complexNumbers[0].B * complexNumbers[1].B;
+                double resultB = complexNumbers[0].A * complexNumbers[1].B + complexNumbers[0].B * complexNumbers[1].A;
+
+                return new ComplexNumber(resultA, resultB);
+            }
+
+            public ComplexNumber Divide()
+            {
+                double denominator = complexNumbers[1].A * complexNumbers[1].A + complexNumbers[1].B * complexNumbers[1].B;
+
+                if (denominator == 0)
+                {
+                    throw new InvalidOperationException("Невозможно выполнить операцию деления на ноль.");
+                }
+
+                double resultA = (complexNumbers[0].A * complexNumbers[1].A + complexNumbers[0].B * complexNumbers[1].B) / denominator;
+                double resultB = (complexNumbers[0].B * complexNumbers[1].A - complexNumbers[0].A * complexNumbers[1].B) / denominator;
+
+                return new ComplexNumber(resultA, resultB);
+            }
+        }
+    }
+}
--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,11 +30,17 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBoxFirstA = new System.Windows.Forms.TextBox();
+            this.textSecondA = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxFirstB = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxSecondB = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -43,9 +49,9 @@
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label1.Location = new System.Drawing.Point(12, 48);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(273, 29);
+            this.label1.Size = new System.Drawing.Size(313, 29);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Комплексное число 1:";
+            this.label1.Text = "Комплексное число 1: Z =";
             // 
             // label2
             // 
@@ -53,36 +59,39 @@
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label2.Location = new System.Drawing.Point(12, 90);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(273, 29);
+            this.label2.Size = new System.Drawing.Size(313, 29);
             this.label2.TabIndex = 1;
-            this.label2.Text = "Комплексное число 2:";
+            this.label2.Text = "Комплексное число 2: Z =";
             // 
-            // textBox1
+            // textBoxFirstA
             // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox1.Location = new System.Drawing.Point(293, 45);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(200, 35);
-            this.textBox1.TabIndex = 2;
+            this.textBoxFirstA.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxFirstA.Location = new System.Drawing.Point(322, 45);
+            this.textBoxFirstA.Name = "textBoxFirstA";
+            this.textBoxFirstA.Size = new System.Drawing.Size(62, 35);
+            this.textBoxFirstA.TabIndex = 2;
+            this.textBoxFirstA.Text = "a";
+            this.textBoxFirstA.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
-            // textBox2
+            // textSecondA
             // 
-            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox2.Location = new System.Drawing.Point(293, 87);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(200, 35);
-            this.textBox2.TabIndex = 3;
+            this.textSecondA.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textSecondA.Location = new System.Drawing.Point(322, 87);
+            this.textSecondA.Name = "textSecondA";
+            this.textSecondA.Size = new System.Drawing.Size(62, 35);
+            this.textSecondA.TabIndex = 3;
+            this.textSecondA.Text = "a";
+            this.textSecondA.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.button1.Location = new System.Drawing.Point(293, 128);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(200, 37);
+            this.button1.Size = new System.Drawing.Size(217, 37);
             this.button1.TabIndex = 4;
             this.button1.Text = "Выполнить";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // comboBox1
             // 
@@ -107,18 +116,83 @@
             this.label3.Size = new System.Drawing.Size(353, 29);
             this.label3.TabIndex = 6;
             this.label3.Text = "Комплексное число Z = a + bi";
-            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(390, 48);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 29);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "+";
+            // 
+            // textBoxFirstB
+            // 
+            this.textBoxFirstB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxFirstB.Location = new System.Drawing.Point(420, 45);
+            this.textBoxFirstB.Name = "textBoxFirstB";
+            this.textBoxFirstB.Size = new System.Drawing.Size(62, 35);
+            this.textBoxFirstB.TabIndex = 8;
+            this.textBoxFirstB.Text = "b";
+            this.textBoxFirstB.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(491, 48);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(19, 29);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "i";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(390, 90);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(27, 29);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "+";
+            // 
+            // textBoxSecondB
+            // 
+            this.textBoxSecondB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxSecondB.Location = new System.Drawing.Point(420, 86);
+            this.textBoxSecondB.Name = "textBoxSecondB";
+            this.textBoxSecondB.Size = new System.Drawing.Size(62, 35);
+            this.textBoxSecondB.TabIndex = 11;
+            this.textBoxSecondB.Text = "b";
+            this.textBoxSecondB.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(491, 90);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(19, 29);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "i";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1258, 744);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBoxSecondB);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxFirstB);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textSecondA);
+            this.Controls.Add(this.textBoxFirstA);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
@@ -132,11 +206,17 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBoxFirstA;
+        private System.Windows.Forms.TextBox textSecondA;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxFirstB;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxSecondB;
+        private System.Windows.Forms.Label label7;
     }
 }
 
